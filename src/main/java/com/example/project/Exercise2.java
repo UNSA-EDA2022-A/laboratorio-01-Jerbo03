c7820ea870c939da2e048352dabb3a69a1fe2c1a
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-05-07 11:35:51 -0500

    Quité caracteres no permitidos

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -19,10 +19,10 @@ public class Exercise2 {
 	}
 
 	public Integer getMenorNumeroSaltos(int a []) {
-		int prev = 0; // Almacenará los valores anteriores a cada salto
+		int prev = 0; // Almacenara los valores anteriores a cada salto
 		for (int i = 0; i < a.length; i++) {
 			//Numero de saltos necesarios para llegar a la distancia a[i], menos 1 salto: a[i] / 50
-			if (a[i] / 50 - prev > 1) return -1;  // Si de la anterior distancia a la actual se necesitan más de un salto, entonces es imposible
+			if (a[i] / 50 - prev > 1) return -1;  // Si de la anterior distancia a la actual se necesitan mas de un salto, entonces es imposible
 			prev = a[i] / 50;
 		}
 		return a[a.length - 1] / 50 + 1; //La distancia requerida para llegar al final siempre y cuando se halla un camino en medio
