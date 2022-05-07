bb5aa73d550c573444f8bc6aadff664f7ff22e4e
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-05-07 11:34:35 -0500

    Algoritmo 1

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -9,20 +9,22 @@ public class Exercise2 {
 		Exercise2 obj = new Exercise2();
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-
-            int a [] = new int [7];
-            for(int i = 0; i < 7; i++){
-                int n = sc.nextInt();
-                a[i] = n;
-            }
-			
+			int a [] = new int [7];
+			for(int i = 0; i < 7; i++){
+				int n = sc.nextInt();
+				a[i] = n;
+			}
 			System.out.println(obj.getMenorNumeroSaltos(a));			
 		}
 	}
 
 	public Integer getMenorNumeroSaltos(int a []) {
-
-		// TO DO
-		return -1;
+		int prev = 0; // Almacenará los valores anteriores a cada salto
+		for (int i = 0; i < a.length; i++) {
+			//Numero de saltos necesarios para llegar a la distancia a[i], menos 1 salto: a[i] / 50
+			if (a[i] / 50 - prev > 1) return -1;  // Si de la anterior distancia a la actual se necesitan más de un salto, entonces es imposible
+			prev = a[i] / 50;
+		}
+		return a[a.length - 1] / 50 + 1; //La distancia requerida para llegar al final siempre y cuando se halla un camino en medio
 	}
 }
